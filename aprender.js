4c7db346a500a72b4bcb2d189280f4efe7a75dfb
JIsaquewillers <joaoisaquewillers@gmail.com>
2025-11-10 12:20:05 -0300

    aula js

---
+++ aprender.js
@@ -0,0 +1,9 @@
+//variável que recebe a largura da tela da classe window
+let largura = window.screen.width;
+//alerta na janela  o texto
+window.alert("A largura da tela é" + largura);
+
+let titulo = document.getElementById("titulo");
+console.log(titulo);
+
+document.getElementById("titulo").innerHTML = "isaque";
--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 console.log("Ola mundo");
 alert("Meu site");
+
